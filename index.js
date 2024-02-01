0fbdec2fda8d41e6f70e00858adbef2292b6eb84
nonameShijian <2954700422@qq.com>
2024-02-01 20:05:38 +0800

    使未找到的文件不返回html

--- index.js
+++ index.js
@@ -134,6 +134,10 @@ try {
 		}
 	});
 
+	app.use((req, res, next) => {
+		res.status(404).send("Sorry can't find that!");
+	})
+
 	app.use(function (err, req, res, next) {
 		console.log(err);
 		return res.json(failedJson(400, String(err)));
